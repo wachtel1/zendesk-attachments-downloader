4f5f265caef4de7b2244513908d08abb9c89f31e
_was <wachtelbird@gmail.com>
2022-06-11 00:52:19 +1000

    moved to persistant

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
 let ticketId = "";
-let pathTemplate = template`Tickets/${'ticketId'}/${'filename'}`;
 
-export function template(strings, ...keys) {
+function template(strings, ...keys) {
   return ((...values) => {
     let dict = values[values.length - 1] || {};
     if (keys.length === 0) {
       for (let i = 1; i < strings.length; i++) {
         keys.push(strings[i]);
-      }
+      };
       strings = strings[0];
-    }
+    };
     let result = [strings[0]];
     keys.forEach((key, i) => {
       let value = Number.isInteger(key) ? values[key] : dict[key];
@@ -17,7 +16,7 @@ export function template(strings, ...keys) {
     });
     return result.join('');
   });
-}
+};
 
 function deconstructTemplate(string) {
   const re = /\$\{\'\w{1,}\'\}/gm;
@@ -29,59 +28,51 @@ function deconstructTemplate(string) {
     template.push(element);
   });
   return template;
-}
+};
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install" || details.reason === "update") {
     let url = chrome.runtime.getURL("options.html");
     chrome.tabs.create({ url });
-  }
-})
-
-chrome.runtime.onStartup.addListener(() => {
-  chrome.storage.sync.get(['pathTemplate'], (result) => {
-    if (result.pathTemplate) {
-      pathTemplate = template(deconstructTemplate(result.pathTemplate));
-    } else {
-      pathTemplate = template`Tickets/${'ticketId'}/${'filename'}`;
-    }
-  })
-  return true;
-})
-
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  if ("pathTemplate" in changes) {
-    pathTemplate = template(deconstructTemplate(changes.pathTemplate.newValue));
-  }
-  return true;
-})
+    chrome.storage.local.set({ pathTemplate: "Tickets/${'ticketId'}/${'filename'}" });
+  };
+});
 
 chrome.tabs.onActivated.addListener((info) => {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     if (tabs[0] && tabs[0].url.includes("zendesk.com/agent/tickets/")) {
       ticketId = tabs[0].url.split("/")[5];
-    }
-  })
+    };
+  });
   return true;
-})
+});
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     if (tabs[0] && tabs[0].id === tabId) {
       if (tab.url && tab.url.includes("zendesk.com/agent/tickets/")) {
         ticketId = tab.url.split("/")[5];
-      }
-    }
-  })
+      };
+    };
+  });
   return true;
-})
+});
 
 chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
   if (item.url.includes("zendesk.com/")) {
+    console.log("download from Zendesk");
+    let pathTemplate = template`Tickets/${'ticketId'}/${'filename'}`;
+    chrome.storage.local.get(['pathTemplate'], (result) => {
+      if (result.pathTemplate) {
+        console.log(result);
+        pathTemplate = template(deconstructTemplate(result.pathTemplate));
+      };
+    });
     const filename = pathTemplate({ ticketId: ticketId, filename: item.filename });
+    console.log(filename);
     suggest({ filename: filename });
   } else {
     suggest()
-  }
+  };
   return true;
-})
+});
--- options.js
+++ options.js
@@ -1,30 +1,23 @@
-import { template } from "./background.js";
-
-const defaultTemplate = template`Tickets/${'ticketId'}/${'filename'}`;
-
 function save_options() {
-    var pathTemplate = document.getElementById('pathTemplate').value;
-    chrome.storage.sync.set({
+    let pathTemplate = document.getElementById('pathTemplate').value;
+    chrome.storage.local.set({
         pathTemplate: pathTemplate
     }, () => {
-        var status = document.getElementById('status');
+        let status = document.getElementById('status');
         status.textContent = 'Options saved.';
         setTimeout(() => {
             status.textContent = '';
         }, 2000);
     });
-}
+};
 
 function restore_options() {
-    const defaultTemplateString = defaultTemplate({
-        ticketId: "${'ticketId'}", filename: "${'filename'}"
-    });
-    chrome.storage.sync.get({
-        pathTemplate: defaultTemplateString
+    chrome.storage.local.get({
+        pathTemplate: "Tickets/${'ticketId'}/${'filename'}"
     }, (items) => {
         document.getElementById('pathTemplate').value = items.pathTemplate;
     });
-}
+};
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
